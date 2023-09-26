413585d3ccda5bad2e2d1d98c304523c68825eed
jenny <gulruxermatova12345@gmail.com>
2023-09-26 23:52:56 +0500

    hamma fayllarni gitga qo'shdik

---
+++ bin/najot_talim_crm_2.dart
@@ -0,0 +1,5 @@
+import 'package:najot_talim_crm_2/najot_talim_crm_2.dart' as najot_talim_crm_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${najot_talim_crm_2.calculate()}!');
+}
---
+++ test/najot_talim_crm_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:najot_talim_crm_2/najot_talim_crm_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
